5ba7f6ad957b4bb205fdb3686faf3b8558f5abcc
Luis Blanco <raubtierxxx@gmail.com>
2017-01-30 10:34:52 +0300

    fix

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 const os = require('os');
 
 const addonPaths = {
-	win32 : '/bin_win32/webgl' ,
-	linux : '/bin_linux/webgl' ,
-	darwin: '/bin_darwin/webgl',
+	win32 : '/bin_win32' ,
+	linux : '/bin_linux' ,
+	darwin: '/bin_darwin',
 };
 
 process.env.path += ';' + __dirname + addonPaths[os.platform()];
